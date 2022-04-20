ced2fd959d36fddae41fb7d6fe320aabd84d52c6
Angeles Cahill <angeles.cahill@gmail.com>
2022-04-20 10:50:24 -0400

    subiendo archivos

---
+++ assets/js/script.js
@@ -0,0 +1,44 @@
+$(document).ready(function () {
+    $("#mensaje").click(function () {
+        alert("Tu mensaje ha sido enviado exitosamente!!")
+    });
+
+    $("#ver-1").click(function () {
+        $(".modal").show()
+    });
+    $("#cerrar-modal").click(function () {
+        $(".modal").hide()
+    });
+
+    $(document).ready(function () {
+        $(".modal-2").hide();
+    })
+
+    $("#ver-2").click(function () {
+        $(".modal-2").show()
+    });
+    $("#cerrar-modal-2").click(function () {
+        $(".modal-2").hide()
+    });
+
+    $(document).ready(function () {
+        $(".modal-3").hide();
+    })
+
+    $("#ver-3").click(function () {
+        $(".modal-3").show()
+    });
+    $("#cerrar-modal-3").click(function () {
+        $(".modal-3").hide()
+    });
+    $(".icono-1").click(function (){
+        $("#lorem-1").slideToggle()   
+    });
+     $(".icono-2").click(function (){
+        $("#lorem-2").slideToggle()   
+    });
+     $(".icono-3").click(function (){
+        $("#lorem-3").slideToggle()   
+    });
+
+});
